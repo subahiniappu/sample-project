9b95d77395bf4dd2ad0175188992b812daf2f8c2
subahiniappu <47855007+subahiniappu@users.noreply.github.com>
2019-08-02 15:15:24 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,5 +1,5 @@
  function studentController($scope,$http) {
-            var url = "/data.txt";
+            var url = "/data1.txt";
 
             $http.get(url).then( function(response) {
                $scope.students = response.data;
